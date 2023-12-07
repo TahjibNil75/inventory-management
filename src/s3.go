1e960eea82c37b9c047761ae45c4bddaed08468a
Tahjib <141258712+TahjibNil75@users.noreply.github.com>
2023-12-07 22:24:31 +0600

    Update s3.go

--- src/s3.go
+++ src/s3.go
@@ -35,3 +35,49 @@ func uploadToS3(data []byte, key string) error {
 	})
 	return err
 }
+
+func CheckIfAssetExistsinS3(assetTag string) bool {
+	// Correct the S3 key for the asset's QR code.
+	key := "qrcode/" + assetTag + ".png" // Fix the typo here
+	// Aws session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("AWS_ACCESS_KEY_ID"),
+			os.Getenv("AWS_SECRET_ACCESS_KEY"),
+			"",
+		),
+	})
+	if err != nil {
+		return false
+	}
+	s3Client := s3.New(sess)
+
+	_, err = s3Client.HeadObject(&s3.HeadObjectInput{
+		Bucket: aws.String(os.Getenv("AWS_BUCKET")),
+		Key:    aws.String(key),
+	})
+	return err == nil
+}
+
+func DeleteFromS3(key string) error {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("AWS_ACCESS_KEY_ID"),
+			os.Getenv("AWS_SECRET_ACCESS_KEY"),
+			"",
+		),
+	})
+	if err != nil {
+		return err
+	}
+
+	s3Client := s3.New(sess)
+	_, err = s3Client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(os.Getenv("BUCKET_NAME")),
+		Key:    aws.String(key),
+	})
+	return err
+}
+
