3b13223dff1e249833116dfa3ad4fa2f2562f245
Tahjib <141258712+TahjibNil75@users.noreply.github.com>
2023-12-07 22:26:24 +0600

    Feat: Added UpdateQrCode

--- src/handler.go
+++ src/handler.go
@@ -28,3 +28,58 @@ func GenerateAndUploadQRCode(ctx *gin.Context) {
 	})
 
 }
+
+func UpdateUploadQRCode(ctx *gin.Context) {
+	var qrCodeParams models.QRCodeParams
+	if err := ctx.ShouldBindJSON(&qrCodeParams); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "failed to update qrcode",
+		})
+		return
+	}
+	// Check if the asset with the given tag is already uploaded
+	if !CheckIfAssetExistsinS3(qrCodeParams.AssetTag) {
+		err := DeleteFromS3("qrcode/" + qrCodeParams.AssetTag + ".png")
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"message": "Failed to delete previos qrcode",
+			})
+			return
+		}
+	}
+	// Generate the updated qrcode
+	qrCode := GenerateQRCode(qrCodeParams)
+
+	// Upload to s3
+	err := uploadToS3(qrCode, "qrcode/"+qrCodeParams.AssetTag+".png")
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to upload qrcode",
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "qrcode updated successfully",
+	})
+}
+
+// func DeleteQRCode(ctx *gin.Context) {
+// 	assetTag := ctx.Param("assetTag")
+
+// 	if !CheckIfAssetExistsinS3(assetTag) {
+// 		ctx.JSON(http.StatusNotFound, gin.H{
+// 			"message": "Asset not found",
+// 		})
+// 		return
+// 	}
+// 	err := DeleteFromS3("qrcode/" + assetTag + ".png")
+// 	if err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{
+// 			"message": "Failed to delete qrcode",
+// 		})
+// 		return
+// 	}
+// 	ctx.JSON(http.StatusOK, gin.H{
+// 		"message": "qrcode deleted successfully",
+// 	})
+// }
